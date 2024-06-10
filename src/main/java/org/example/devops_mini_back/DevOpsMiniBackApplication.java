3136f30dbcf66e1b76e2beb8c6eda0a3be940ca5
01022549653@naver.com <>
2024-06-10 16:45:05 +0900

    first commit

---
+++ src/main/java/org/example/devops_mini_back/DevOpsMiniBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.devops_mini_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsMiniBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsMiniBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/devops_mini_back/DevOpsMiniBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devops_mini_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsMiniBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
